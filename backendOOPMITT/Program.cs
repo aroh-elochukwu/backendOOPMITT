5b65fdca73f815221c2cb8cc23f9094028bf507e
Elochukwu Aroh <elochukwuaroh@gmail.com>
2022-06-24 08:36:59 -0500

    class 1

--- backendOOPMITT/Program.cs
+++ backendOOPMITT/Program.cs
@@ -1,2 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Calendar myCalendar = new Calendar();
+
+Date halloween = new Date("Halloween", 31);
+myCalendar.Dates.Add(halloween);
+Console.WriteLine(myCalendar.Dates.First().Title);
+
+
+class Calendar
+{
+    public string Name { get; set; }
+    public int year { get; set; }   
+    public List<Date> Dates { get; set; }
+
+    public Calendar()
+    {
+        Name = "Year 2000";
+        year = 2000;
+        Dates = new List<Date>();
+    }
+
+    //Overload
+
+    public Calendar(string name, int year)
+    {
+        Name = name;
+        year = year;
+        Dates = new List<Date>();
+    }
+}
+
+
+
+class Date
+{
+    public string Title { get; set; }
+    private int _dayOfMonth { get; set; }
+
+    public int DayOfMonth
+    {
+        get { return _dayOfMonth; }
+        set
+        {
+            if(value > 0 && value <= 31)
+            {
+                _dayOfMonth = value;
+            }
+        }
+       
+    }
+
+    public Date(string title, int day)
+    {
+        Title = title;
+        DayOfMonth = day;
+    }
+
+}
